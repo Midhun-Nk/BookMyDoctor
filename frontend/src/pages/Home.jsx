a874730343b8445f3a99d263811ae8110a4c9cf1
Midhun NK <midhunnk2021@gmail.com>
2025-08-31 14:40:29 +0530

    Home Page Updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,14 @@
+import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctors from "../components/TopDoctors";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <SpecialityMenu />
+      <TopDoctors />
+    </div>
+  );
 };
 export default Home;
