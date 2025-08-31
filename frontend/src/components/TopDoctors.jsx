80d309e69895f90fd7c32def12fed6b13b1c1c8b
Midhun NK <midhunnk2021@gmail.com>
2025-08-31 19:36:21 +0530

    Doctor Page Updated

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,6 +1,10 @@
-import { doctors } from "../assets/assets_frontend/assets";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
 
 const TopDoctors = () => {
+  const navigate = useNavigate();
+  const { doctors } = useContext(AppContext);
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
       <h1 className="text-3xl font-medium">Top Doctoes to Book</h1>
@@ -12,6 +16,7 @@ const TopDoctors = () => {
           <div
             className="border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
+            onClick={() => navigate(`/appointment/${item._id}`)}
           >
             <img className="bg-blue-50 not-visited:" src={item.image} alt="" />
             <div
@@ -22,13 +27,21 @@ const TopDoctors = () => {
                 <p className="w-2 h-2 bg-green-500 rounded-full"></p>
                 <p>Available</p>
               </div>
-              <p>{item.name}</p>
-              <p>{item.speciality}</p>
+              <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+              <p className="text-gray-600 text-sm ">{item.speciality}</p>
             </div>
           </div>
         ))}
       </div>
-      <button>more</button>
+      <button
+        onClick={() => {
+          navigate("/doctors");
+          scrollTo(0, 0);
+        }}
+        className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10"
+      >
+        more
+      </button>
     </div>
   );
 };
