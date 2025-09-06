bbb579100b64b76b25c09b325cc4117dca97af5a
Midhun NK <midhunnk2021@gmail.com>
2025-09-06 12:25:18 +0530

    Bug Fixed Logout

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -170,12 +170,12 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className="flex flex-col gap-2 justify-end">
-              {!item.cancelled && item.payment && (
+              {!item.cancelled && item.payment && !item.isCompleted && (
                 <button className="sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50 ">
                   Paid
                 </button>
               )}
-              {!item.cancelled && !item.payment && (
+              {!item.cancelled && !item.payment && !item.isCompleted && (
                 <button
                   className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300"
                   onClick={() => appointmentRazorpay(item._id)}
@@ -184,7 +184,7 @@ const MyAppointments = () => {
                 </button>
               )}
 
-              {!item.cancelled && (
+              {!item.cancelled && !item.isCompleted && (
                 <button
                   className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300"
                   onClick={() => cancelAppointment(item._id)}
@@ -192,9 +192,17 @@ const MyAppointments = () => {
                   Cancel appointment
                 </button>
               )}
-              {item.cancelled && (
+              {item.cancelled && !item.isCompleted && (
                 <button className="sm:min-w-48 border border-red-500 rounded text-red-500">
                   Appointment cancelled
+                </button>
+              )}
+              {item.isCompleted && (
+                <button
+                  className="sm:min-w-48 border 
+                py-2 border-green-500 rounded text-green-500"
+                >
+                  Appointment completed
                 </button>
               )}
             </div>
